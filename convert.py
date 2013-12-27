be3b3bfb003ec98d7e3a5b419eaeabba46aec59a
Eitan Mosenkis <eitan@mosenkis.net>
2013-12-26 19:57:57 -0800

    Add support for server-side filters; add --module to add modules; lots of cleanup

--- convert.py
+++ convert.py
@@ -5,6 +5,7 @@ from __future__ import (division, absolute_import, print_function,
 
 import argparse
 import collections
+import importlib
 import inspect
 import io
 import logging
@@ -16,28 +17,55 @@ from xml.dom import minidom
 import html5lib
 
 LOGGER = logging.getLogger(__name__)
+LOG_LEVELS = [
+    logging.WARNING,
+    logging.INFO,
+    logging.DEBUG,
+]
+HTML5_VOID_ELEMENTS = set((
+    'area',
+    'base',
+    'br',
+    'col',
+    'embed',
+    'hr',
+    'img',
+    'input',
+    'keygen',
+    'link',
+    'menuitem',
+    'meta',
+    'param',
+    'source',
+    'track',
+    'wbr',
+))
 
 
 def main(args):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--infile', required=True)
-    parser.add_argument('--outfile', type=file, default=sys.stdout)
-    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--infile',
+                        type=argparse.FileType('r'),
+                        default=sys.stdin)
+    parser.add_argument('--outfile',
+                        type=argparse.FileType('w'),
+                        default=sys.stdout)
+    parser.add_argument('--verbose', '-v', action='count', default=0)
+    parser.add_argument('--module', action='append', default=[])
+    parser.add_argument('--ng', type=bool, default=True)
     args = parser.parse_args(args)
-    if args.debug:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=LOG_LEVELS[min(args.verbose,
+                                             len(LOG_LEVELS) - 1)])
     LOGGER.info('Converting %s => %s',
-                args.infile, args.outfile)
-    converter = Converter()
-    converter.module(noscript)
-    with open(args.infile, 'rb') as f:
-        document = converter.Convert(f)
-    xml = document.toxml(encoding='utf-8')
-    # Strip off XML declaration
-    xml = xml[xml.index(b'>') + 1:]
-    args.outfile.write(xml)
+                args.infile.name, args.outfile.name)
+    converter = Converter(loadNg=args.ng)
+    for module in args.module:
+        parts = module.split('.')
+        pymodule = '.'.join(parts[:-1])
+        name = parts[-1]
+        LOGGER.info('Loading module %s from %s', name, pymodule)
+        converter.module(getattr(importlib.import_module(pymodule), name))
+    args.outfile.write(converter.Convert(args.infile))
 
 
 class Converter(object):
@@ -62,10 +90,13 @@ class Converter(object):
     def config(self, directive, key, value):
         self._directive_config[directive][key] = value
 
-    def Convert(self, f):
-        document = self.parser.parse(f)
-        self._convert(document, document.documentElement)
-        return document
+    def Convert(self, input):
+        document = self.parser.parse(input)
+        self._convert_element(document, document.documentElement)
+        xml = document.toxml(encoding='utf-8')
+        # Strip off XML declaration
+        xml = xml[xml.index(b'>') + 1:]
+        return xml
 
     def ConvertExpr(self, text):
         # TODO(eitan): be more nuanced about replacing $, &&, !, ||, etc.
@@ -91,8 +122,8 @@ class Converter(object):
                 parts[i] = args[0]
         return '|'.join(parts)
 
-    def _convert_text_node(self, document, element):
-        data = element.wholeText
+    def _convert_text_node(self, document, node):
+        data = node.wholeText
         out = io.StringIO()
         i = 0
         while i < len(data):
@@ -109,21 +140,16 @@ class Converter(object):
             out.write('}}')
             i = end + 2
         out.write(data[i:])
-        element.replaceWholeText(out.getvalue())
+        node.replaceWholeText(out.getvalue())
 
-    def _convert(self, document, element):
-        if element.nodeType == minidom.Node.TEXT_NODE:
-            return self._convert_text_node(document, element)
-        elif element.nodeType != minidom.Node.ELEMENT_NODE:
-            # TODO(eitan): handle text nodes and comments appropriately
-            return
+    def _convert_element(self, document, element):
         tagName = self._normalize_name(element.tagName)
         attrs = self._get_attribute_map(element)
         # TODO(eitan): convert ngAttrFoo -> foo
-        LOGGER.debug('%s node has attrs %s', tagName, attrs.keys())
         for directive in self._directives:
-            LOGGER.debug('Processing directive %s', directive.name)
             if 'A' in directive.restrict and directive.name in attrs:
+                LOGGER.debug('Applying directive %s to <%s> for attribute',
+                             directive.name, element.tagName)
                 value = element.getAttribute(attrs[directive.name])
                 directive(
                     document=document,
@@ -135,6 +161,8 @@ class Converter(object):
                     value=value,
                 )
             if 'E' in directive.restrict and directive.name == tagName:
+                LOGGER.debug('Applying directive %s for element',
+                             directive.name)
                 directive(
                     document=document,
                     element=element,
@@ -146,13 +174,22 @@ class Converter(object):
             if element.parentNode is None:
                 return
         # Must slurp into list because elements may be removed during iteration
-        # TODO(eitan): something safer for modifying during iteration
+        # TODO(eitan): something safer for modifying during iteration - maybe
+        #              non-recursive
         for child in list(element.childNodes):
-            self._convert(document, child)
-        # Avoid invalid self-closing tags by inserting a comment in empty tags
-        # TODO(eitan): check against list of HTML5 bodyless tags
-        if not element.childNodes:
-            element.appendChild(document.createComment('nocollapse'))
+            if child.parentNode != element:
+                continue
+            if child.nodeType == minidom.Node.TEXT_NODE:
+                self._convert_text_node(document, child)
+            elif child.nodeType == minidom.Node.ELEMENT_NODE:
+                self._convert_element(document, child)
+            # TODO(eitan): handle comments appropriately
+        # Minidom will produce self-closing tags whenever it prints an empty
+        # element. In HTML5, the self-closing status is not honored except in
+        # the case of 'foreign elements' (SVG and MathML). To force separate
+        # open and close tags, add an empty comment.
+        if not (tagName in HTML5_VOID_ELEMENTS or element.childNodes):
+            element.appendChild(document.createComment(''))
 
     def _get_attribute_map(self, element):
         if element.attributes:
@@ -187,6 +224,7 @@ class Module(object):
     def __init__(self, name):
         self.name = name
         self.directives = []
+        self.filters = []
 
     def directive(self,
                   name=None,
@@ -197,6 +235,18 @@ class Module(object):
             self.directives.append(directive)
             return func
         return decorator
+
+    def filter(self,
+               name=None):
+        def decorator(func):
+            _name = func.__name__ if name is None else name
+            self.filters.append((_name, func))
+            return func
+        return decorator
+
+    def apply_to_jinja_env(self, env):
+        for name, filter in self.filters:
+            env.filters[name] = filter
 
     class Directive(object):
         def __init__(self,
